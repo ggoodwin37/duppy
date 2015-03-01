82cb856c0442ba489011df37dc6c9182605c09c4
ggoodwin37 <ggoodwin37@gmail.com>
2015-02-28 20:07:34 -0800

    WIP

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var glob = require('glob');
+var manyFileHashes = require('many-file-hashes');
+var async = require('async');
+
+// TODO: from command line.
 var basePath = './sample-data/base';
 var comparePath = './sample-data/compare';
 
@@ -6,10 +10,52 @@ var globOptions = {
 	nodir: true
 };
 
-glob(basePath + '/**', globOptions, function(err, files) {
-	if (err) {
-		console.log('glob error: ' + err);
-		return;
+var baseHashes = {};
+
+async.series({
+	getBaseHashes: function(cb) {
+		glob(basePath + '/**', globOptions, function(globErr, files) {
+			if (globErr) {
+				console.log('globErr: ' + globErr);
+				return cb();
+			}
+			manyFileHashes(files, function(hashErr, hashes) {
+				if (hashErr) {
+					console.log('hashErr: ' + hashErr);
+					return cb();
+				}
+				hashes = (hashes || []).map(function(thisHash) {
+					return thisHash.hash;
+				});
+				hashes.forEach(function(thisHash) {
+					baseHashes[thisHash] = true;
+				});
+				cb();
+			});
+		});
+	},
+	getCompareHashes: function(cb) {
+		glob(comparePath + '/**', globOptions, function(globErr, files) {
+			if (globErr) {
+				console.log('globErr: ' + globErr);
+				return cb();
+			}
+			manyFileHashes(files, function(hashErr, hashes) {
+				if (hashErr) {
+					console.log('hashErr: ' + hashErr);
+					return cb();
+				}
+				hashes.forEach(function(thisHashResult) {
+					if (baseHashes[thisHashResult.hash]) {
+						console.log('compare file is dupe: ' + thisHashResult.original);
+					} else {
+						console.log('compare file is NOT dupe: ' + thisHashResult.original);
+					}
+				});
+				cb();
+			});
+		});
 	}
-	console.log('glob results: ' + (files || []).join(', '));
+}, function() {
+	console.log('all done');
 });
