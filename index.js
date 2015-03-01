38e686944af9609f95ac1e12dca1ba9ec3047410
ggoodwin37 <ggoodwin37@gmail.com>
2015-02-28 19:40:56 -0800

    Glob

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+var glob = require('glob');
+var basePath = './sample-data/base';
+var comparePath = './sample-data/compare';
 
+var globOptions = {
+	nodir: true
+};
+
+glob(basePath + '/**', globOptions, function(err, files) {
+	if (err) {
+		console.log('glob error: ' + err);
+		return;
+	}
+	console.log('glob results: ' + (files || []).join(', '));
+});
